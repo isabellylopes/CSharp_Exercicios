459b29c4f105266d10240fc7392b54101d821c63
Isabelly Lopes <77798720+isabellylopes@users.noreply.github.com>
2022-10-26 14:59:48 -0300

    upload

---
+++ Conta/ContaCorrente.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Titular;
+
+namespace ByteBank.Conta
+{
+    public class ContaCorrente // Representação de uma conta corrente
+    {
+        public  static int QuantidadeContas { get; private set; }
+
+
+        // variáveis
+        private int num_agencia; // privando variavel
+
+
+        // cria propriedade que é uma forma de acessar uma variavel privada, ao inves de criar um metodo
+        public int Num_agencia
+        {
+            get { return this.num_agencia; }
+            private set { 
+                    if(value > 0)
+                    {
+                        this.num_agencia = value; 
+                    }         
+                
+                }
+        }
+
+        //public string conta;
+
+        //Criando propriedade autoincrementada, onde é a variavel é definidade dentro da propriedade
+
+        public string Conta { get; set;}
+
+        private double saldo; // Privando a variavel saldo para que não seja possivel modifica-lá fora da classe
+
+        private Cliente titular { get; set; }
+
+        // metódos
+        public void Depositar(double valor) // não irá retornar valor
+        {
+            saldo += valor; // esse saldo, do objeto em si, vai receber ele + o valor de deposito
+        }
+
+        public bool Saque(double valor) // não irá retornar valor
+        {
+            if (valor <= saldo)
+            {
+                saldo -= valor; // esse saldo, do objeto em si, vai recever ele - o valor de saque
+                return true;
+
+            }
+
+            else { return false; }
+
+        }
+
+        public bool Tranferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+
+            }
+
+            else
+            {
+                Saque(valor);
+                destino.Depositar(valor);
+                return true;
+            }
+        }
+
+        // criando um metodo para poder modificar o saldo que está privado e não pode ser alterado no programa principal
+        private void SetSaldo(double valor)
+        {
+            if (valor < 0)
+            {
+                return;
+            }
+            else
+            {
+                this.saldo = valor;
+            }
+        }
+
+        // criando um metodo para exibir o saldo que está privado e não pode ser visualizado no programa principal
+        public double GetSaldo()
+        {
+            return this.saldo;
+        }
+
+        // criando metodo construtor, ou seja, as regras da classe
+
+        public ContaCorrente (int numeor_agencia, string numero_conta, double saldo)
+        {
+            this.Num_agencia = numeor_agencia;
+            this.Conta = numero_conta;
+            this.SetSaldo(saldo);
+            QuantidadeContas++;
+
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using ByteBank.Conta;
+using ByteBank.Titular;
+
+//// Conta Isabely
+//ContaCorrente ContaIsabelly = new ContaCorrente();
+//Cliente clienteIsabelly = new Cliente();
+
+//ContaIsabelly.titular = clienteIsabelly;
+
+
+//clienteIsabelly.nome = "Isabelly Lopes";
+//clienteIsabelly.cpf = "123456789";
+//clienteIsabelly.profissao = "Programadora";
+//ContaIsabelly.conta = "1010-X";
+//ContaIsabelly.num_agencia = 3010;
+//ContaIsabelly.saldo = 100.00;
+
+
+//// Conta Nataly
+
+//ContaCorrente ContaNataly = new ContaCorrente();
+//Cliente clienteNataly = new Cliente();
+
+//ContaNataly.titular = clienteNataly;
+
+//clienteNataly.nome = "Nataly Pecelin";
+//clienteNataly.cpf = "123456845";
+//clienteNataly.profissao = "Engenheira";
+//ContaNataly.conta = "1020-7";
+//ContaNataly.num_agencia = 2;
+//ContaNataly.saldo = 250.00;
+
+
+//// Movimentações conta Isabelly
+
+//    Console.WriteLine("Titular da conta: "+ clienteIsabelly.nome);
+//    Console.WriteLine("CPF: " + clienteIsabelly.cpf);
+//    Console.WriteLine("Saldo atual da conta: "+ ContaIsabelly.saldo);
+
+//    ContaIsabelly.Depositar(50.00);
+
+//    Console.WriteLine("Esse é o valor após deposito: "+ ContaIsabelly.saldo);
+
+//    if(ContaIsabelly.Saque(300.00) == true)
+//        {
+//            Console.WriteLine("Esse é o valor após saque "+ ContaIsabelly.saldo);
+//        }
+
+
+//    else 
+//    { 
+//            Console.WriteLine("Não pode sacar, Saldo insuficiente"); 
+//    }
+
+
+//Console.WriteLine("");
+
+
+//// Movimentações conta Nataly
+//Console.WriteLine("Titular da conta: "+clienteNataly.nome);
+//Console.WriteLine("CPF: " + clienteNataly.cpf);
+
+//Console.WriteLine("Saldo da Nataly antes da tranferencia: "+ ContaNataly.saldo);
+
+
+//Console.WriteLine("");
+
+
+//// Tranferindo da conta da Isabelly para a conta da Nataly
+//ContaIsabelly.Tranferir(25.00, ContaNataly);
+
+//Console.WriteLine("Esse é o valor da Isabelly após a transação: " + ContaIsabelly.saldo);
+
+//Console.WriteLine("");
+
+
+//Console.WriteLine("Esse é o valor da Nataly após a transação " + ContaNataly.saldo);
+
+
+//ContaCorrente conta3 = new ContaCorrente(15, "1547-P", -1);
+
+//Cliente cliente = new Cliente();
+
+//Console.WriteLine(conta3.GetSaldo());
+//Console.WriteLine(conta3.Num_agencia);
+//Console.WriteLine(conta3.Conta);
+//Console.WriteLine(cliente.Nome);
+
+
+ContaCorrente conta5 = new ContaCorrente(1, "154-O", 50);
+ContaCorrente conta6 = new ContaCorrente(14, "154-P", 50);
+
+Console.WriteLine(ContaCorrente.QuantidadeContas);
